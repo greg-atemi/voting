271f1a3375d3baf48c624da665c3cf5fe473611a
Greg Atemi <magrega7180@gmail.com>
2024-04-13 22:53:40 +0300

    Refactored app to match requirements

--- polls/views.py
+++ polls/views.py
@@ -1,45 +1,45 @@
 from django.db.models import F
 from django.http import Http404
 from django.urls import reverse
-from . models import Choice, Question
+from . models import Candidate, Election
 from django.shortcuts import get_object_or_404, render
-from django.http import HttpResponse, HttpResponseRedirect
+from django.http import HttpResponseRedirect
 
 
 def index(request):
-    latest_question_list = Question.objects.order_by("-pub_date")
-    context = {"latest_question_list": latest_question_list}
+    election_list = Election.objects.order_by("-pub_date")
+    context = {"election_list": election_list}
     return render(request, "polls/index.html", context)
 
-def detail(request, question_id):
+def detail(request, election_id):
     try:
-        question = Question.objects.get(pk=question_id)
-    except Question.DoesNotExist:
-        raise Http404("Question does not exist")
-    return render(request, "polls/detail.html", {"question": question})
+        election = Election.objects.get(pk=election_id)
+    except Election.DoesNotExist:
+        raise Http404("Election does not exist")
+    return render(request, "polls/detail.html", {"election": election})
 
-def results(request, question_id):
-    question = get_object_or_404(Question, pk=question_id)
-    return render(request, "polls/results.html", {"question": question})
+def results(request, election_id):
+    election = get_object_or_404(Election, pk=election_id)
+    return render(request, "polls/results.html", {"election": election})
 
-def vote(request, question_id):
-    question = get_object_or_404(Question, pk=question_id)
+def vote(request, election_id):
+    election = get_object_or_404(Election, pk=election_id)
     try:
-        selected_choice = question.choice_set.get(pk=request.POST["choice"])
-    except (KeyError, Choice.DoesNotExist):
-        # Redisplay the question voting form.
+        selected_candidate = election.candidate_set.get(pk=request.POST["candidate"])
+    except (KeyError, Candidate.DoesNotExist):
+        # Redisplay the election voting form.
         return render(
             request,
             "polls/detail.html",
             {
-                "question": question,
-                "error_message": "You didn't select a choice.",
+                "election": election,
+                "error_message": "You didn't select a candidate.",
             },
         )
     else:
-        selected_choice.votes = F("votes") + 1
-        selected_choice.save()
+        selected_candidate.votes = F("votes") + 1
+        selected_candidate.save()
         # Always return an HttpResponseRedirect after successfully dealing
         # with POST data. This prevents data from being posted twice if a
         # user hits the Back button.
-        return HttpResponseRedirect(reverse("polls:results", args=(question.id,)))
+        return HttpResponseRedirect(reverse("polls:results", args=(election.id,)))
